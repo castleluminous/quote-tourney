6ea5bff7141140bcb5272f595c2348550567256b
chat-eau-lumi <tourmaline.kittiwake@gmail.com>
2025-10-15 11:34:13 -0400

    actual features, pretty code, etc

--- src/App.tsx
+++ src/App.tsx
@@ -7,21 +7,21 @@ import QuoteTable from './QuoteTable';
 import TheArena from './TheArena';
 
 function App() {
-const [value, setValue] = useState("1")
+  const [value, setValue] = useState("1")
   const handleChange = (_event: React.SyntheticEvent, newValue: string) => {
     setValue(newValue);
   };
 
   type Quote = {
-      number: number;
-      quote: string;
+    number: number;
+    quote: string;
   };
   const quoteList: Quote[] = jsonQuotes as Quote[];
 
   return (
     <Box sx={{ width: '100%', typography: 'body1' }}>
-      <h1>stupid late night coding adventure</h1>
-      <h3>todo: like a bunch</h3>
+      <h1>the big 'ol quote tournament</h1>
+      <h3>you can put acknowledgements and stuff here or a funny little joke idc</h3>
       <TabContext value={value}>
         <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
           <TabList onChange={handleChange}>
@@ -29,8 +29,8 @@ const [value, setValue] = useState("1")
             <Tab label="All Quotes" value="2" />
           </TabList>
         </Box>
-        <TabPanel value="1">{<TheArena rows={quoteList}/>}</TabPanel>
-        <TabPanel value="2">{<QuoteTable rows={quoteList}/>}</TabPanel>
+        <TabPanel value="1">{<TheArena rows={quoteList} />}</TabPanel>
+        <TabPanel value="2">{<QuoteTable rows={quoteList} />}</TabPanel>
       </TabContext>
     </Box>
   );
--- src/TheArena.tsx
+++ src/TheArena.tsx
@@ -1,26 +1,44 @@
-import { Button, Card, CardContent } from "@mui/material";
-import { useEffect, useState } from "react";
+import { Accordion, AccordionDetails, AccordionSummary, Button, Card, CardContent, Typography } from "@mui/material";
+import { useLayoutEffect, useState } from "react";
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 
 type Quote = {
     number: number;
     quote: string;
 };
 
-function TheArena(props: {rows: Quote[]}) {
-    const [nextRoundQuotes, setNextRoundQuotes] = useState<Quote[]>([])
-    const [currentRoundQuotes, setCurrentRoundQuotes] = useState<Quote[]>(props.rows)
-    const [currentFightQuotes, setCurrentFightQuotes] = useState<Quote[]>([])
-    const [winner, setWinner] = useState<Quote>()
+function TheArena(props: { rows: Quote[] }) {
+    const next = localStorage.getItem('nextRoundQuotes')
+    const current = localStorage.getItem('currentRoundQuotes')
+    const fight = localStorage.getItem('currentFightQuotes')
+    const round = localStorage.getItem('numRounds')
+    const loadWin = localStorage.getItem('winner')
+    const [nextRoundQuotes, setNextRoundQuotes] = useState<Quote[]>(next ? JSON.parse(next) : [])
+    const [currentRoundQuotes, setCurrentRoundQuotes] = useState<Quote[]>(current ? JSON.parse(current) : [])
+    const [currentFightQuotes, setCurrentFightQuotes] = useState<Quote[]>(fight ? JSON.parse(fight) : [])
+    const [numRounds, setNumRounds] = useState(round ? JSON.parse(round) : 1)
+    const [winner, setWinner] = useState<Quote | undefined>(loadWin && loadWin !== "undefined" ? JSON.parse(loadWin) : undefined)
+    const [firstRender, setFirstRender] = useState(true);
+
 
     const resetStuff = () => {
         setNextRoundQuotes([])
         setCurrentRoundQuotes(props.rows)
         setCurrentFightQuotes([])
+        setNumRounds(1)
         setWinner(undefined)
     }
 
+    const saveProgress = () => {
+        localStorage.setItem("nextRoundQuotes", JSON.stringify(nextRoundQuotes));
+        localStorage.setItem("currentRoundQuotes", JSON.stringify(currentRoundQuotes));
+        localStorage.setItem("currentFightQuotes", JSON.stringify(currentFightQuotes));
+        localStorage.setItem("numRounds", JSON.stringify(numRounds));
+        localStorage.setItem("winner", JSON.stringify(winner));
+    }
+
     const startStuff = () => {
-        const currentQuotes : Quote[] = []
+        const currentQuotes: Quote[] = []
         const numQuotes = Math.min(5, currentRoundQuotes.length)
         for (var i = 0; i < numQuotes; i++) {
             currentQuotes.push(currentRoundQuotes[i])
@@ -29,29 +47,58 @@ function TheArena(props: {rows: Quote[]}) {
         setCurrentFightQuotes(currentQuotes)
     }
 
-    const selectWinner = (quote : Quote) => {
+    const selectWinner = (quote: Quote) => {
         setNextRoundQuotes([...nextRoundQuotes, quote])
         setCurrentFightQuotes([])
     }
 
-    useEffect(() => {
-        if (currentRoundQuotes.length > 0){
-        startStuff()}
+    useLayoutEffect(() => {
+        if (firstRender) { setFirstRender(false); return; };
+        if (currentRoundQuotes.length > 0) {
+            startStuff()
+        }
         else if (currentRoundQuotes.length === 0 && nextRoundQuotes.length > 1) {
             setCurrentRoundQuotes(nextRoundQuotes)
             setNextRoundQuotes([])
+            setNumRounds(numRounds + 1)
         }
         else {
             setWinner(nextRoundQuotes[0])
         }
     }, [nextRoundQuotes]);
 
-  return (
-    <div>
-    <Button onClick={resetStuff}>reset</Button>
-    {currentFightQuotes.length !== 0 && currentFightQuotes.map((quote) => <Card style={{padding: "10px"}}><CardContent>Quote # {quote.number}</CardContent> {quote.quote} <CardContent><Button onClick={() => selectWinner(quote)}>Select This Quote</Button></CardContent></Card>)}
-    {winner && <Card style={{padding: "10px"}}>Winner: {winner.number} {winner.quote}</Card>}
-    </div>
+    return (
+        <div>
+            <Button onClick={resetStuff}>reset the whole thing (does not override save, but still, careful!)</Button>
+            <h2>Round {numRounds}</h2>
+            <Button onClick={saveProgress}>save your progress</Button>
+            <div style={{ padding: "10px" }}>to load progress, refresh page. it's weird. i know. i'm lazy :(</div>
+            {currentFightQuotes.length !== 0 && currentFightQuotes.map((quote) => <Card style={{ padding: "10px" }}><CardContent><b>Quote #{quote.number}</b></CardContent> {quote.quote} <CardContent><Button onClick={() => selectWinner(quote)}>Select This Quote</Button></CardContent></Card>)}
+            {winner && <Card style={{ padding: "10px" }}><CardContent><b>Winner:</b></CardContent><CardContent><b>Quote #{winner.number}</b></CardContent> {winner.quote} </Card>}
+            <h3>misc. stuff</h3>
+            <Accordion>
+                <AccordionSummary
+                    id="next"
+                    expandIcon={<ExpandMoreIcon />}
+                >
+                    <Typography component="span">Quotes in the next round</Typography>
+                </AccordionSummary>
+                <AccordionDetails>
+                    {nextRoundQuotes.map((quote) => quote.number + ": " + quote.quote).join(" ~ ")}
+                </AccordionDetails>
+            </Accordion>
+            <Accordion>
+                <AccordionSummary
+                    id="next"
+                    expandIcon={<ExpandMoreIcon />}
+                >
+                    <Typography component="span">Quotes in the current round (careful, might be chunky)</Typography>
+                </AccordionSummary>
+                <AccordionDetails>
+                    {currentRoundQuotes.map((quote) => quote.number + ": " + quote.quote).join(" ~ ")}
+                </AccordionDetails>
+            </Accordion>
+        </div>
     )
 }
 
